52c403faa2a1f784666f4dec0bfffe1888aeec2a
zhangsan <979738371@qq.com>
2019-01-24 16:30:36 +0800

    Jenkins  1.0版本

---
+++ scripts/test_ab.py
@@ -0,0 +1,21 @@
+import allure
+def test_a():
+    print('aaa')
+    assert 1
+
+@allure.step(title='测试登录的流程')
+def test_b():
+    allure.attach('登录','输入用户名')
+    print('bbb')
+    allure.attach('登录','输入密码')
+    print('ccc')
+    allure.attach('登录','点击登录')
+    assert 0
+
+
+
+
+
+
+
+
